Fixed overflow errors in exp/log

mlp_train.py
@@ -1,10 +1,10 @@
 import numpy as np, copy, math, itertools, cProfile, pstats
 
-def sigmoid(matrix):
+def sigmoid(matrix, maxval = 1e100):
     """Apply the sigmoid function to a matrix."""
     # Correct for overflow
     max_val = np.log(np.finfo(matrix.dtype).max)-1e-4
-    matrix_clipped = np.clip(matrix, None, max_val)
+    matrix_clipped = np.clip(matrix, -max_val, max_val)
     return 1/(1+np.exp(-matrix_clipped))
 
 def add_bias(matrix):
@@ -65,8 +65,11 @@ def compute_gradients(weights_list, inputs, activations_list, deltas_list,
 
 def sigmoid_cost(outputs, labels, weights_list, regulariser):
     """Compute the cost function of a sigmoid MLP net."""
-    inner_cost = -np.sum(np.nan_to_num(labels*np.log(outputs) + \
-            (1-labels)*np.log(1-outputs)))
+    max_val = np.finfo(outputs.dtype).max
+    min_val = 1/max_val
+    pos_cost = labels * np.log(np.clip(outputs, min_val, max_val))
+    neg_cost = (1-labels)*np.log(np.clip(1-outputs, min_val, max_val))
+    inner_cost = -np.sum(pos_cost + neg_cost)
     reg_cost = sum([np.sum(zero_bias(weights_list[n])**2) \
             for n in range(len(weights_list))]) * regulariser / 2
     return((inner_cost + reg_cost)/len(labels))